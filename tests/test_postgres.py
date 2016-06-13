f6b634c07dfd960f7090fbd6ac128454407d958b
Sebastian Neubauer <sebineubauer@gmail.com>
2016-06-13 23:50:41 +0200

    first test proofing successful restore from s3

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -37,24 +37,29 @@ class DockerUnittestExample(unittest.TestCase):
         cur.execute('''CREATE TABLE COMPANY
                (ID INT PRIMARY KEY     NOT NULL,
                NAME           TEXT    NOT NULL,
-               AGE            INT     NOT NULL,
-               ADDRESS        CHAR(50),
-               SALARY         REAL);''')
+               AGE            INT     NOT NULL);''')
+        cur.execute('''INSERT INTO COMPANY VALUES (1,'MAX',30);''')
         print "Table created successfully"
 
         conn.commit()
         conn.close()
-        self.project.stop('postgres')
-        self.project.rm('postgres')
-        self.project.up('postgres')
+        for service in self.project.get_services():
+            print service.name
+        self.project.stop(service_names=["postgres"])
+        self.project.remove_stopped(['postgres'])
+        self.project.up(['postgres'])
         wait_for_postgres(dbname='postgres', user="postgres", password="postgres", host="127.0.0.1", port="8432")
-        time.sleep(60)
+        #time.sleep(60)
+        conn = psycopg2.connect(dbname='postgres', user="postgres", password="postgres", host="127.0.0.1", port="8432")
+        print "Opened database successfully"
+
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+        cur = conn.cursor()
         cur = conn.cursor()
         cur.execute('select * from COMPANY')
         rows = cur.fetchall()
-        self.assertEqual(rows,[])
-        #Test something with postgres
-        self.fail()
+        self.assertEqual(rows,[(1, 'MAX', 30)])
 
 
 if __name__ == '__main__':
